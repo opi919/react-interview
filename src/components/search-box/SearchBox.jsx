ec2f49162ca8ff53200fadf7acf08ca7ca69abf6
opi919 <khandokermomen919@gmail.com>
2023-10-03 19:20:46 +0600

    removed unwanted variables

--- src/components/search-box/SearchBox.jsx
+++ src/components/search-box/SearchBox.jsx
@@ -7,42 +7,6 @@ import FormItem from "../form/FormItem"
 export default function SearchBox() {
   const { formData, handleChange } = useContext(SearchContext)
 
-  const [loadingFromOptions, setLoadingFromOptions] = useState(false)
-  const [loadingToOptions, setLoadingToOptions] = useState(false)
-
-  const [fromOptions, setFromOptions] = useState([])
-  const [toOptions, setToOptions] = useState([])
-
-  const currentDate = new Date().toISOString().split("T")[0]
-
-  const getDefaultReturnDate = () => {
-    const tomorrow = new Date()
-    tomorrow.setDate(tomorrow.getDate() + 1)
-    return tomorrow.toISOString().split("T")[0]
-  }
-
-  const loadFromOptions = () => {
-    if (!fromOptions.length) {
-      setLoadingFromOptions(true)
-      setFromOptions(CITIES)
-
-      setTimeout(() => {
-        setLoadingFromOptions(false)
-      }, 1000)
-    }
-  }
-
-  const loadToOptions = () => {
-    if (!toOptions.length) {
-      setLoadingToOptions(true)
-
-      setTimeout(() => {
-        setToOptions(["City A", "City B", "City C"])
-        setLoadingToOptions(false)
-      }, 1000)
-    }
-  }
-
   const searchFlights = () => {
     const searchParams = new URLSearchParams(formData).toString()
     const url = `/search?${searchParams}`
@@ -53,7 +17,6 @@ export default function SearchBox() {
     <div className="container">
       <form>
         <Menu />
-
         <div className="row">
           <FormItem title="from" />
           <FormItem title="to" />
